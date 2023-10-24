80aaa898c9141ca6965a59fec062f290f09395bf
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2023-10-24 16:14:01 -0300

    los otros 7 ejercicios terminados

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -8,7 +8,10 @@ const resta = numero1 - numero2
 const multiplicacion = numero1 * numero2
 const division = numero1 / numero2
 
+console.log("EJERCICIO 1:")
 console.log("El resultado de la suma entre " + numero1 + " y " + numero2 + " es: " + suma);
 console.log("El resultado de la resta entre " + numero1 + " y " + numero2 + " es: " + resta);
 console.log("El resultado de la multiplicación entre " + numero1 + " y " + numero2 + " es: " + multiplicacion);
 console.log("El resultado de la división entre " + numero1 + " y " + numero2 + " es: " + division);
+
+
---
+++ js/ejercicio2.js
@@ -0,0 +1,8 @@
+/* Ejercicio 2: Concatenación de Cadenas */
+
+const nombre = "Dylan "
+const apellido = "Navarro"
+const nombreCompleto = nombre + apellido
+
+console.log("EJERCICIO 2:")
+console.log("El nombre completo es: " + nombreCompleto)
